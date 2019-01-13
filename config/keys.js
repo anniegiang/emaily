2cc66f1eb19dab38e9bf95770aca7d024596de31
Annie Giang <Annie@Annies-MacBook-Pro.local>
2019-01-13 15:40:12 -0800

    removed keys.js from gitignore file

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	TwitterConsumerKey: 'NJYRcAZegS1Haj5PvTSn6A03y',
+	TwitterConsumerSecret: 'baHMS5esxNNLY9bilKpwm8WsoLCt02RNfiZhjnzPGOssscxYtk',
+	mongoURI: 'mongodb://annieg4123:annie123@ds255794.mlab.com:55794/music-chart-viewer',
+	cookieKey: 'vneirunvidslnkvj'
+}
