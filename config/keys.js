59881b7983f70c803ec051b63fd00d14e24f1af0
Annie Giang <Annie@Annies-MacBook-Pro.local>
2019-01-13 16:02:09 -0800

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-	TwitterConsumerKey: 'NJYRcAZegS1Haj5PvTSn6A03y',
-	TwitterConsumerSecret: 'baHMS5esxNNLY9bilKpwm8WsoLCt02RNfiZhjnzPGOssscxYtk',
-	mongoURI: 'mongodb://annieg4123:annie123@ds255794.mlab.com:55794/music-chart-viewer',
-	cookieKey: 'vneirunvidslnkvj'
+if(process.env.NODE_ENV === 'production') {
+	module.exports = require('./prod.js');
+} else {
+	module.exports = require('./dev.js');
 }
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	TwitterConsumerKey: process.env.TWITTER_CONFIG_KEY,
+	TwitterConsumerSecret: process.env.TWITTER_CONFIG_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
+}
